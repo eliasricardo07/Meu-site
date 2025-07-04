231a230a2a52837876ca3385061989bb72cb7d4d
eliasricardo07 <eliasricardo5005@gmail.com>
2025-07-04 14:40:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function voltarPagina() {
+  // Redireciona para o Google com página em branco (sem pesquisa)
+  window.location.href = "https://www.google.com";
+}
+
+// Adiciona classe para disparar a animação após o carregamento da página
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+});
+
+// Função para botão voltar já existente
+function voltarPagina() {
+  window.location.href = "https://www.google.com";
+}
+
